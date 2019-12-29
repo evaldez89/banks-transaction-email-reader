59938024e1930fb20c85879ae41585bc27c1d181
Emmanuel Valdez <evaldez89@gmail.com>
2019-12-28 20:31:37 -0400

    Add method to get suscribed banks base on the base class children

--- banks_mail_readers/__init__.py
+++ banks_mail_readers/__init__.py
@@ -1,1 +1,15 @@
 from . import vimenca_reader, bhdleon_reader
+# TODO: Posiblemente hacer los imports de los bancos
+# ya no sea necesario debido a que se esta haciendo
+# dinamicamente
+
+
+def get_subscribed_banks() -> list:
+    from .base_reader import BaseReader
+    banks = list()
+    for child in BaseReader.__subclasses__():
+        banks.append({
+            'main_class': child.__name__,
+            'module': child.__module__
+        })
+    return banks
