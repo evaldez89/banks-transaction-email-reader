783cade82c017fc3da2bc67aef2e94a901dc9ddc
Emmanuel Valdez <evaldez89@gmail.com>
2019-12-28 21:18:38 -0400

    fix vimenca class init method

--- banks_mail_readers/vimenca_reader.py
+++ banks_mail_readers/vimenca_reader.py
@@ -2,6 +2,5 @@ from .base_reader import BaseReader
 
 
 class VimencaHtmlReader(BaseReader):
-    def __init__(self, days_to_read, raw_html):
-        self._query = ''
-        return super().__init__('internetbanking@vimenca.com', days_to_read, raw_html)
+    def __init__(self):
+        return super().__init__('internetbanking@vimenca.com')
