ea808291011a436801059c3b665bdecd2ca82cef
Emmanuel Valdez <evaldez89@gmail.com>
2020-01-08 23:39:59 -0400

    [ADD]  necessary logic to depend only on the message factory and not the bank factory

--- banks_mail_readers/message_factory.py
+++ banks_mail_readers/message_factory.py
@@ -1,8 +1,22 @@
-from . import vimenca_messages, bhdleon_messages
 import importlib
+
+from . import bhdleon_messages, vimenca_messages
+from .message import MessageAbs
 
 
 class MessageFactory():
+
+    _available_messages_modules: list = list()
+
+    def __init__(self):
+        for child in MessageAbs.__subclasses__():
+            self._available_messages_modules.append({
+                'main_class': child.__name__,
+                'module': child.__module__
+            })
+
+        super().__init__()
+
     @staticmethod
     def get_bank_messages(bank_name) -> list:
         messages_module_info = MessageFactory.__bank_messages_module_info(bank_name)
@@ -21,12 +35,22 @@ class MessageFactory():
 
     @staticmethod
     def __bank_messages_module_info(bank_name: str) -> list:
-        from .message import MessageAbs
         messages = list()
-        for child in MessageAbs.__subclasses__():
-            if child.bank == bank_name:
-                messages.append({
-                    'main_class': child.__name__,
-                    'module': child.__module__
-                })
+        for modules_info in MessageFactory._available_messages_modules:
+            module_path = modules_info.get('module', False)
+            if bank_name == MessageFactory.extract_bank_name(module_path):
+                messages.append(modules_info)
         return messages
+
+    @staticmethod
+    def extract_bank_name(module_path):
+        if module_path:
+            module_path = module_path.split('.')
+            module_path = module_path[1] if len(module_path) == 2 else module_path
+            module_path = module_path.split('_')[0]
+
+        return module_path
+
+    @staticmethod
+    def get_subscribed_banks():
+        return MessageFactory._available_messages_modules
