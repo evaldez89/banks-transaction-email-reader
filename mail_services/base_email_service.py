d031a59d4dbd7ee57f8adae1464a97715b077a6e
Emmanuel Valdez <evaldez89@gmail.com>
2020-01-22 20:43:05 -0400

    change email service to use message factory internally and to construct the message detail line

--- mail_services/base_email_service.py
+++ mail_services/base_email_service.py
@@ -4,17 +4,21 @@ from datetime import date, datetime, timedelta
 from typing import Any  # TODO: Create base interface for email service to stop using 'Any' as a type
 
 from banks_mail_readers.message_abs import MessageAbs
+from banks_mail_readers.message_factory import MessageFactory
 
 
 class EmailService(ABC):
 
     date_to = date.today() + timedelta(1)
+    messages_factory = MessageFactory()
 
-    def __init__(self, message_template: MessageAbs, days_from: int):
+    def __init__(self, bank_name: str, days_from: int):
         self.name = 'Base Service'
+        self.bank_name = bank_name
         self.credentials = None
         self.service: Any = None
-        self.message_template = message_template
+        # self.message_template = message_template
+
         # End date must be tomorrow in order to
         # To Ensure all messages (including today) al fetched
         self.date_from = self.date_to - timedelta(days_from)
@@ -22,20 +26,8 @@ class EmailService(ABC):
         self.query = f'before:{self.date_to:%Y/%m/%d} ' \
                      f'after:{self.date_from:%Y/%m/%d}'
 
-    @abstractmethod
     def construct_query(self):
         pass
-
-    def decode_message_body(self, encoded_data: str):
-        """Decode message body to be able to parse it to HTML.
-
-        Arguments:
-            encoded_data {str} -- base64 encode html message
-
-        Returns:
-            str -- decoded html message
-        """
-        return urlsafe_b64decode(encoded_data)
 
     @abstractmethod
     def authenticate(self):
@@ -52,3 +44,28 @@ class EmailService(ABC):
     @abstractmethod
     def read_mail(self):
         pass
+
+    def decode_message_body(self, encoded_data: str):
+        """Decode message body to be able to parse it to HTML.
+
+        Arguments:
+            encoded_data {str} -- base64 encode html message
+
+        Returns:
+            str -- decoded html message
+        """
+        return urlsafe_b64decode(encoded_data)
+
+    def get_message_details(self, message_body: str, subject: str):
+        message_class = EmailService.messages_factory.get_bank_message_template(self.bank_name, subject)
+        if message_class is not None:
+            message_template = message_class()
+            message_template.feed(self.decode_message_body(message_body))
+
+            line = f"{message_template.date}|{message_template.currency}|"
+            line += f"{message_template.amount}|{message_template.merchant}|"
+            line += f"{message_template.status}|{message_template.type}\n"
+
+            return line
+        print(subject)
+        return False
